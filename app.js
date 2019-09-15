912fc2a1395d7d6b74a822f72a687ab8ae0c02a3
Johnny Tsang <jaytsang9@gmail.com>
2019-09-15 19:14:12 -0400

    config app

---
+++ app.js
@@ -0,0 +1,49 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override"),
+    flash           = require("connect-flash"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    port            = process.env.PORT || 3000;
+
+// DB CONFIG
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/restful_blog_app";
+mongoose.connect(url, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true});
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useFindAndModify: false});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// Passport config
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Passes the currentUser data into every route so it drys up the code
+// Use this to display username on headers or webpages, etc.
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+})
+
+
+app.listen(port, function(){
+    console.log("App is starting!!!");
+});
