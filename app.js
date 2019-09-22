e0d5e23de4ee734aa52f5817dc7da685ae5cc99f
Johnny Tsang <jaytsang9@gmail.com>
2019-09-22 02:29:16 -0400

    refactor app file

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-const express         = require("express"),
-    app             = express(),
-    bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose"),
-    passport        = require("passport"),
-    LocalStrategy   = require("passport-local"),
-    methodOverride  = require("method-override"),
-    flash           = require("connect-flash"),
-    Post            = require("./models/post"),
-    Comment         = require("./models/comment"),
-    User            = require("./models/user"),
-    seedDB          = require("./seeds"),
-    port            = process.env.PORT || 3000;
-    
+const express          = require("express"),
+      app              = express(),
+      bodyParser       = require("body-parser"),
+      expressSanitizer = require("express-sanitizer"),
+      mongoose         = require("mongoose"),
+      passport         = require("passport"),
+      LocalStrategy    = require("passport-local"),
+      methodOverride   = require("method-override"),
+      flash            = require("connect-flash"),
+      Post             = require("./models/post"),
+      Comment          = require("./models/comment"),
+      User             = require("./models/user"),
+      commentRoutes    = require("./routes/comments"),
+      postRoutes       = require("./routes/posts"),
+      indexRoutes      = require("./routes/index"),
+      port             = process.env.PORT || 3000;
 
-const commentRoutes = require("./routes/comments"),
-    postRoutes = require("./routes/posts"),
-    indexRoutes = require("./routes/index");    
-
-// DB CONFIG
-let url = process.env.DATABASEURL || "mongodb://localhost:27017/nba_app";
-mongoose.connect(url, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true});
-
+mongoose.connect("mongodb://localhost:27017/nba_app", {useNewUrlParser: true, useFindAndModify: false});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
 app.use(methodOverride("_method"));
 app.use(flash());
+app.use(expressSanitizer());
 
-// Tell app to use momentjs to timestamp posts and replies
-app.locals.moment = require("moment");
+//require moment and add to app.locals
+app.locals.moment = require('moment');
 
 // Passport config
 app.use(require("express-session")({
-    secret: "Bentley is the cutest!",
+    secret: "Once again Rusty wins cutest dog!",
     resave: false,
     saveUninitialized: false
 }));
@@ -52,10 +47,15 @@ app.use((req, res, next) => {
     next();
 })
 
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+});
+
 app.use("/", indexRoutes);
 app.use("/posts", postRoutes);
 app.use("/posts/:id/comments", commentRoutes);
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log("App is starting!!!");
 });
