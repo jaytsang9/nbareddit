ef5801ba6bd1b0032348238ed8bd91aae5ab51b2
Johnny Tsang <jaytsang9@gmail.com>
2019-09-22 02:35:14 -0400

    refactor user model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
-var mongoose = require("mongoose"),
+const mongoose = require("mongoose"),
     passportLocalMongoose = require("passport-local-mongoose"),
-    userSchema = new mongoose.Schema({
+    UserSchema = new mongoose.Schema({
         username: String,
         password: String
     });
 
-userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User", userSchema); 
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", UserSchema);
